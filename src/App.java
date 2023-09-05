dee9d00ebceefbb3f80bdd341a7b0f5ffea06ba1
sergipe085 <dapscreed@gmail.com>
2023-09-05 10:18:02 -0300

    gauss

--- src/App.java
+++ src/App.java
@@ -4,13 +4,16 @@ public class App {
     public static void main(String[] args) throws Exception {
         Scanner sc = new Scanner(System.in);
 
-        Matriz matrizTeste = new Matriz(2, 2);
+        Matriz matrizTeste = new Matriz(3, 4);
         matrizTeste.FillInput(sc);
 
         System.out.println(matrizTeste.ToString());
         
         
-        Matriz transpostaTeste = LinearAlgebra.Tranposta(matrizTeste);
-        System.out.println(transpostaTeste.ToString());
+        // Matriz transpostaTeste = LinearAlgebra.Tranposta(matrizTeste);
+        // System.out.println(transpostaTeste.ToString());
+
+        Matriz gaussTeste = LinearAlgebra.Gauss(matrizTeste);
+        System.out.println(gaussTeste.ToString());
     }
 }
--- src/LinearAlgebra.java
+++ src/LinearAlgebra.java
@@ -13,4 +13,32 @@ public class LinearAlgebra {
 
         return transposta;
     }
+
+    public static Matriz Gauss(Matriz matriz) {
+        Matriz solucao = new Matriz(matriz.GetLinhas(), 1);
+
+        int n = matriz.GetLinhas();
+
+        // Fase de Eliminação
+        for (int k = 0; k < n - 1; k++) {
+            for (int i = k + 1; i < n; i++) {
+                double fator = matriz.GetValue(i, k) / matriz.GetValue(k, k);
+                for (int j = k; j < n + 1; j++) {
+                    matriz.SetValue(i, j, matriz.GetValue(i, j) - fator * matriz.GetValue(k, j));
+                }
+            }
+        }
+
+        // Fase de Retrosubstituição
+        solucao.SetValue(n - 1, 0, matriz.GetValue(n-1, n) / matriz.GetValue(n-1, n-1));
+        for (int i = n - 2; i >= 0; i--) {
+            double soma = 0.0;
+            for (int j = i + 1; j < n; j++) {
+                soma += matriz.GetValue(i, j) * solucao.GetValue(j, 0);
+            }
+            solucao.SetValue(i, 0, (matriz.GetValue(i, n) - soma) / matriz.GetValue(i, i));
+        }
+
+        return solucao;
+    }
 }
